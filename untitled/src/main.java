d5a9203878493265ea8b5bfed349197a62e8389c
AdrianCzarnecki <79456196+AdrianCzarnecki@users.noreply.github.com>
2021-02-26 10:07:54 +0100

    praktyki 2gg

--- untitled/src/main.java
+++ untitled/src/main.java
@@ -1,25 +1,45 @@
-public class main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        //zad1
+public class main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean programIsRunning = true;
 
-        char letterA = 'A';
+        while(programIsRunning)
+        {
+            System.out.print("Podaj numer drzwi: ");
+            String userInput = sc.next();
 
-        for (int i=0; i<26; i++){
-            if( i % 2 ==0)
-                System.out.print(letterA + "\t");
-            letterA++;
+            switch (userInput)
+            {
+                case "1": {
+                    System.out.println("Złe drzwi >:)");
+                    break;
+                }
+                case "2": {
+                    System.out.println("Złe drzwi >:)");
+                    break;
+                }
+                case "3": {
+                    programIsRunning = false;
+                    break;
+                }
+                case "4": {
+                    System.out.println("Złe drzwi >:)");
+                    break;
+                }
+                case "5": {
+                    System.out.println("Złe drzwi >:)");
+                    break;
+                }
+                default: {
+                    System.out.println("Podałeś złe dane!");
+                }
+            }
         }
-        //zad2
-        int x=11;
-        int inta=0;
-        int intb=1;
-        for (int i=0; i <x; i++){
-            System.out.print(intb + " ");
-            intb += inta;
-            inta = intb-inta;
-        }
 
+        System.out.println("Bravo skończyłeś lochy!");
     }
-
 }
